d9d06bbf417953c55ff9fe2ebf0053521714f90e
EvilDeedsIM <evildeedsim@gmail.com>
2021-12-06 15:56:29 +0200

    ..

---
+++ modern_architecture/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <navigation></navigation>
+  <hero></hero>
+</template>
+
+<script setup>
+import Navigation from './components/Navigation.vue';
+import Hero from './components/Hero.vue';
+</script>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Outfit:100,200,300,regular,500,600,700,800,900);
+
+html {
+  box-sizing: border-box;
+  font-size: 10px;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  height: 100vh;
+  font-family: Outfit, Arial, sans-serif;
+  overflow: hidden;
+}
+
+ul {
+  list-style: none;
+}
+
+button {
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+  font-family: Outfit, Arial, sans-serif;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ modern_architecture/src/components/Navigation.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="nav">
+    <button class="burger" @click="open = !open">
+      <transition-group name="burger">
+        <Icon icon="ci:menu-alt-05" v-if="open" />
+        <Icon icon="dashicons:menu" v-else />
+      </transition-group>
+    </button>
+
+    <ul class="menu">
+      <li class="menu__item">
+        <a class="menu__link" href="#">offer</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="#">work</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="#">about</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="#">contact</a>
+      </li>
+    </ul>
+
+    <button class="language">english</button>
+  </div>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue';
+import { ref } from 'vue';
+
+const open = ref(false);
+</script>
+
+<style scoped>
+.nav {
+  position: absolute;
+  top: 4rem;
+  left: 0;
+  width: 100%;
+  padding: 0 8rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.2rem;
+  z-index: 10;
+}
+
+.burger {
+  font-size: 3rem;
+  width: 3rem;
+  height: 3rem;
+}
+
+.burger-enter-from {
+  opacity: 0;
+}
+.burger-leave-to {
+  opacity: 0;
+}
+.burger-enter-active {
+  transition: 0.5s 0.5s ease-in-out;
+}
+.burger-leave-active {
+  transition: 0.5s ease-in-out;
+}
+.menu {
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  text-transform: uppercase;
+}
+
+.menu__link {
+  position: relative;
+  text-decoration: none;
+  color: inherit;
+}
+.menu__link::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0;
+  height: 1px;
+  background-color: #000;
+  transition: 0.5s ease-in-out;
+}
+.menu__link:hover::after {
+  width: 100%;
+}
+
+.language {
+  font-family: Outfit, sans-serif;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  color: #fff;
+  text-shadow: 0 0 4px #000;
+  font-weight: 600;
+}
+</style>
